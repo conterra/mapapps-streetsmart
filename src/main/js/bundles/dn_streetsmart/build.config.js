4d4dab2c45670719cf35b05c52eb7515ccd19a8b
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-12-12 14:26:31 +0100

    Update to rollup build

--- src/main/js/bundles/dn_streetsmart/build.config.js
+++ src/main/js/bundles/dn_streetsmart/build.config.js
@@ -13,8 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { StreetSmartController } from "./StreetSmartController";
-export { StreetSmartWidgetFactory } from "./StreetSmartWidgetFactory";
-export { StreetSmartModel } from "./StreetSmartModel";
-export { MarkerController } from "./MarkerController";
-export { MeasurementController } from "./MeasurementController";
+/*
+ This build file, configures information for the rollup-build task:
+ See: https://www.npmjs.com/package/ct-mapapps-gulp-js#user-content-rollup-build
+*/
+module.exports = {
+    // normally the type should be "bundle"
+    type: "bundle",
+    // list all files, which should stay after the build
+    // In this case only the "module.js" is the remaining artifact, all other files will be integrated into this file.
+    entryPoints: ["./module"]
+};
